ce0f87536f0c8712bf22571018a41534c48a814c
pkro <pkrombach@web.de>
2019-11-02 10:46:45 +0100

    basic functionality done

---
+++ script.js
@@ -0,0 +1,50 @@
+useLocal = true;
+useFull = true;
+updateQuoteOnLoad = true;
+
+const quotesRemote = 'https://raw.githubusercontent.com/JamesFT/Database-Quotes-JSON/master/quotes.json';
+const quotesLocal = `quotes${useFull ? '_full' : ''}.json`;
+
+let quotes = [];
+let quotesShownInSession = [];
+const quotesUrl = useLocal ? quotesLocal : quotesRemote;
+
+$(() => {
+    init(quotesUrl, updateQuoteOnLoad)
+});
+
+const init = (quotesURL, callback) => {
+  const request = new XMLHttpRequest();
+  request.onload = () => {
+    quotes = Array.from(JSON.parse(request.responseText));
+    if(updateQuoteOnLoad) {
+      updateQuote(getRandomQuote());
+    }
+    $('#new-quote').click(updateQuote);
+    
+  }
+  request.open('GET', quotesURL);
+  request.send();
+  return request;
+}
+
+
+
+const getRandomQuote = () => {
+  if(quotes.length === 0) {
+    return {quoteText: "I want to kick ass or cite quotes, and I'm all out of quotes.", quoteAuthor: 'This app'}
+  }
+  let quoteIdx = Math.round(Math.random() * quotes.length);
+  
+  quotes.splice(quoteIdx, 1);
+  quotesShownInSession.push(quoteIdx);
+  
+  return quotes[quoteIdx];
+} 
+
+const updateQuote = () => {
+  let quote = getRandomQuote();
+  $('#text').text(quote.quoteText);
+  $('#author').text(quote.quoteAuthor);
+}
+
